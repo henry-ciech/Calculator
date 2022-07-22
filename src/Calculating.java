913b19744b59d6b30d9ecbcfd5ce09a20206b200
Mada1Adam <henryk.ciechanowiec@gmai.com>
2022-07-23 00:46:11 +0300

    my changes

--- src/Calculating.java
+++ src/Calculating.java
@@ -1,27 +1,30 @@
+import java.math.*;
+
 public class Calculating {
-    public void calculating(float firstNum, float secondNum, String operator) {
-        float result;
+    public void calculating(BigDecimal firstNum, BigDecimal secondNum, String operator) {
+        MathContext mc = new MathContext(2); // 2 precision
+        BigDecimal decimalFirstNum = new BigDecimal(String.valueOf(firstNum));
+        BigDecimal decimalSecondNum = new BigDecimal(String.valueOf(secondNum));
+        BigDecimal result;
+        
         switch (operator) {
-            case ("*"):
-                result = firstNum * secondNum;
-                PrintResult printResult1 = new PrintResult(result);
-                printResult1.getResult();
-                break;
-            case ("/"):
-                result = firstNum / secondNum;
-                PrintResult printResult2 = new PrintResult(result);
-                printResult2.getResult();
-                break;
             case ("+"):
-                result = firstNum + secondNum;
-                PrintResult printResult3 = new PrintResult(result);
-                printResult3.getResult();
+                result = decimalFirstNum.add(decimalSecondNum);
+                System.out.println(result);
                 break;
             case ("-"):
-                result = firstNum - secondNum;
-                PrintResult printResult4 = new PrintResult(result);
-                printResult4.getResult();
+                result = decimalFirstNum.subtract(decimalSecondNum, mc);
+                System.out.println(result);
                 break;
+            case ("/"):
+                result = decimalFirstNum.divide(decimalSecondNum);
+                System.out.println(result);
+                break;
+            case ("*"):
+                result = decimalFirstNum.multiply(decimalSecondNum);
+                System.out.println(result);
+                break;
+
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.math.BigDecimal;
 import java.util.*;
 
 public class Main {
@@ -10,25 +11,28 @@ public class Main {
         while(true) {
             print.print("Provide the first number: ");
             String input1 = scanner.nextLine();
-            if (!checkingInput.checkInput(input1) || input1.isEmpty()) {
+            if (!checkingInput.checkInput(input1) || input1.isEmpty() ||  input1.equals("-0")) {
                 print.print("Wrong input");
                 continue;
             }
+            BigDecimal decimalInput1 = new BigDecimal(input1);
 
             print.print("Provide the second number: ");
             String input2 = scanner.nextLine();
-            if (!checkingInput.checkInput(input2) || input2.isEmpty()) {
+            if (!checkingInput.checkInput(input2) || input2.isEmpty() || input1.equals("-0")) {
+                print.print("Wrong input");
+                continue;
+            }
+            BigDecimal decimalInput2 = new BigDecimal(input2);
+
+            print.print("Provide the operator: ");
+            String operator = scanner.nextLine();
+            if (checkingInput.checkInput(operator) || operator.isEmpty() || operator.length() > 1) {
                 print.print("Wrong input");
                 continue;
             }
 
-            print.print("Provide the operator: ");
-            String operator = scanner.nextLine();
-            if (checkingInput.checkInput(operator) || operator.isEmpty()) {
-                continue;
-            }
-
-            calculating.calculating(Integer.valueOf(input1), Integer.valueOf(input2), operator);
+            calculating.calculating(decimalInput1, decimalInput2, operator);
             break;
         }
     }
